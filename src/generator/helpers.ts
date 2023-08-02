a153b4e2b91a0e3ddc11170b74e5bafbbdee9b00
m1212e <14091540+m1212e@users.noreply.github.com>
2023-08-02 12:29:32 +0200

    fix: implement correct isRequired detection

--- src/generator/helpers.ts
+++ src/generator/helpers.ts
@@ -252,7 +252,9 @@ export const generateRelationInput = ({
   // should the validation require the relation field to exist
   // this should only be true in cases where only one relation field is generated
   // for multiple relaiton fields, e.g. create AND connect, each should be optional
-  const isRequired = !(createRelation && connectRelation && disconnectRelation);
+  const isRequired =
+    [createRelation, connectRelation, disconnectRelation].filter((v) => v)
+      .length === 1;
 
   if (createRelation) {
     const preAndPostfixedName = t.createDtoName(field.type);
